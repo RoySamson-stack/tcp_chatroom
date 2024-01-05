95330ae0ac977156d2b8394aa9cf2c3e14d095cb
RoySamson-stack <you@example.com>
2024-01-05 11:21:37 +0300

    adding the function for receiveng the message or the chatroom

--- server.py
+++ server.py
@@ -28,5 +28,34 @@ def handle(client):
             message = client.recv(1024)
             broadcast(message)
         except:
-            index = cl            
+            index = clients.index(client)
+            clients.remove(client)
+            client = close()
+            nickname = nicknames[index]
+            broadcast(f"{nickname} left the chat".encode('ascii'))
+            nickname.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send("Rick".encode('ascii'))
+
+        nickname = client.recv(1024).decode('ascii')
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nicknameof the client is {nickname}!")
+        broadcast(f"{nickname} joined the chat".encode('ascii'))
+        client.send("Connected to the server".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client))
+        thread.start()
+
+
+print("Server started and is listening ")
+receive()        
 
