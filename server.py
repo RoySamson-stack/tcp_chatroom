8ed956cca090759e2932ba14939612a40cba7855
RoySamson-stack <you@example.com>
2024-01-05 10:42:38 +0300

    making the functions to handle the message broadcast and client

--- server.py
+++ server.py
@@ -4,3 +4,29 @@ import sockets
 
 host  = '127.0.0.1'
 port = '99872'
+
+
+server = socket.socket(socket.AF_INET. socket.SOCK_STREAM, socket.SOCK_STREAM)
+server.bind(host, port)
+
+server.listen()
+
+
+clients = []
+nicknames  = []
+
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = cl            
+
